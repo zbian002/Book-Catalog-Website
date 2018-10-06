c2f73b13fc2a57656424a0dea662543d18a7967c
Zhen <zbian002@ucr.edu>
2018-10-05 23:59:17 -0700

    Display data in browser

--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -1,5 +1,8 @@
 from app.catalog import main
+from app import db
+from app.catalog.models import Book, Publication
+from flask import render_template
 
 @main.route('/')
-def hello():
-    return 'hello world'
+def display_books():
+    books = Book.query.all()
--- run.py
+++ run.py
@@ -2,4 +2,6 @@ from app import create_app
 
 if __name__ == '__main__':
     flask_app = create_app('dev')
+    with flask_app.app_context():
+        db.create_all()
     flask_app.run()
