e9047782e0096ff6d09edfaf458a47a5623b5dd3
Zhen <zbian002@ucr.edu>
2018-10-05 23:20:59 -0700

    Delete some folders and download packages

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+# app/__init__.py
+
+import os
+from flask import Flask
+from  flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app(config_type):
+
+    app = Flask(__name__)
+    configuration = os.path.join(os.getcwd(), 'config', config_type + '.py')
+    app.config.from_pyfile(configuration)
+    db.init_app(app)
+
+    from app.catalog import main
+    app.register_blueprint(main)
+
+    return app
