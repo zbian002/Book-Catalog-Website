ad80ffe1d172bf3a55432ccb1e8720e18768bfdc
Zhen <zbian002@ucr.edu>
2018-10-06 23:59:04 -0700

    Install flask-bootstrap package

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 # app/__init__.py
-
 import os
 from flask import Flask
 from  flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 def create_app(config_type):
 
@@ -12,6 +13,7 @@ def create_app(config_type):
     configuration = os.path.join(os.getcwd(), 'config', config_type + '.py')
     app.config.from_pyfile(configuration)
     db.init_app(app)
+    bootstrap.init_app(app)
 
     from app.catalog import main
     app.register_blueprint(main)
