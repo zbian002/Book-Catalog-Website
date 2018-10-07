f06af744dbbe0f9b4cd67691fe3e55970216e9a9
Zhen <zbian002@ucr.edu>
2018-10-07 03:38:45 -0700

    Update app/__init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,13 @@ import os
 from flask import Flask
 from  flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
 
 def create_app(config_type):
 
@@ -14,6 +18,8 @@ def create_app(config_type):
     app.config.from_pyfile(configuration)
     db.init_app(app)
     bootstrap.init_app(app)
+    login_manager.init_app(app)
+    bcrypt.init_app(app)
 
     from app.catalog import main
     app.register_blueprint(main)
