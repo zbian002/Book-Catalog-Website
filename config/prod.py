59fd737cf69350c462dcad4ab74b8f3f23d9a227
Zhen <zbian002@ucr.edu>
2018-10-08 15:39:26 -0700

    make it better

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,6 @@
-DEBUG = True
+import os
+
+DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:020793@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -2,12 +2,11 @@ from app import create_app, db
 from app.auth.models import User
 #from sqlalchemy import exc
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
-        #try:
-            #if not User.query.filter_by(user_name='harry').first():
-                #User.create_user(user='harry', email='harry@potters.com', password='secret')
-        #except exc.IntegrityError:
-        flask_app.run()
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
+    #try:
+        #if not User.query.filter_by(user_name='harry').first():
+            #User.create_user(user='harry', email='harry@potters.com', password='secret')
+    #except exc.IntegrityError:
+    flask_app.run()
