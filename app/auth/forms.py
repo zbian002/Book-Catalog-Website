93149eecce06d68d16772bb6986d394c60748f1b
Zhen <zbian002@ucr.edu>
2018-10-07 17:36:07 -0700

    Add email_exists function in forms.py file

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,6 +3,13 @@ from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.auth.models import User
 
+
+def email_exists(form, field):
+    email = User.query.filter_by(user_email=field.data).first()
+    if email:
+        raise ValidationError('Email Already Exists')
+
+
 class RegistrationForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(3, 15, message='between 3 to 15 characters')])
     email = StringField('E-mail', validators=[DataRequired(), Email(), email_exists])
