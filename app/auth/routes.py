794aa09246a4ace24860925cec87f2d86f6944eb
Zhen <zbian002@ucr.edu>
2018-10-07 19:36:37 -0700

    Add error handling page

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -43,3 +43,8 @@ def log_out_user():
     logout_user()
     flash('Logged out Successfully')
     return redirect(url_for('main.display_books'))
+
+
+@at.app_errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
