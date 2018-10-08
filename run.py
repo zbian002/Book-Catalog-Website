22ca9efeeebe13b41ea8e9ecd446115bbaa0c7e9
Zhen <zbian002@ucr.edu>
2018-10-07 17:09:17 -0700

    Create users in the database

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 from app import create_app, db
+from app.auth.models import User
+from sqlalchemy import exc
 
 if __name__ == '__main__':
     flask_app = create_app('dev')
     with flask_app.app_context():
         db.create_all()
-    flask_app.run()
+        try:
+            if not User.query.filter_by(user_name='harry').first():
+                User.create_user(user='harry', email='harry@potters.com', password='secret')
+        except exc.IntegrityError:
+            flask_app.run()
