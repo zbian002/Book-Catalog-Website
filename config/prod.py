5fee1f616abcb471171f3f0a4587463303444584
Zhen <zbian002@ucr.edu>
2018-10-08 17:53:39 -0700

    make it better

--- config/prod.py
+++ config/prod.py
@@ -2,5 +2,5 @@ import os
 
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:020793@localhost/catalog_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
